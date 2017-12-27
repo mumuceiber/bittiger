6c815d6dc493dbb49b1089c10ea860258750af5a
mumuceiber <mumuceiber@gmail.com>
2017-12-27 13:30:40 -0500

    all four components

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -5,15 +5,26 @@ import { DataService} from './services/data.service';
 
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
+import { routing } from './app.routes';
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
+
+import { FormsModule} from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent,
+    NewProblemComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [
     DataService
---
+++ week1/oj-client/src/app/components/problem-detail/problem-detail.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Problem} from '../../models/problem.model';
+import { DataService} from '../../services/data.service';
+
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-problem-detail',
+  templateUrl: './problem-detail.component.html',
+  styleUrls: ['./problem-detail.component.css']
+})
+export class ProblemDetailComponent implements OnInit {
+  problem: Problem;
+  constructor(private dataService: DataService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.problem = this.dataService.getProblem(+params['id']);
+    });
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
