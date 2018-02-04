1047f962760a191cc218bfd11f8ed49f578572e9
mumuceiber <mumuceiber@gmail.com>
2018-02-03 22:09:20 -0500

    App, Newspanel, and NewsCard component

---
+++ project2/web_server/client/src/NewsPanel/NewsPanel.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import './NewsPanel.css';
+import NewsCard from '../NewsCard/NewsCard';
+import _ from 'lodash';
+
+class NewsPanel extends React.Component {
+  constructor() {
+    super();
+    this.state = { news:null }; // news:list of jason
+  }
+
+  handleScroll() {
+    const scrollY = window.scrollY
+          || window.pageYOffset
+          || document.documentElement.scrollYTop;
+
+    if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+      console.log('Loading more news!');
+      this.loadMoreNews();
+    }
+  }
+
+  // react 本身生命周期里的一个函数
+  // 让render函数结束运行完成之后再运行componentDidMount
+  componentDidMount(){
+    this.loadMoreNews();
+    this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
+    window.addEventListener('scroll', () => this.handleScroll());
+  }
+
+  loadMoreNews(){
+    const news_url = 'http://' + window.location.hostname + ':3000' + '/news';
+    const request = new Request(news_url,{method:'GET',cache:false});
+
+    fetch(request)
+      .then(res=>res.json())
+      .then(news=>{
+        this.setState({
+            news : this.state.news ? this.state.news.concat(news) : news,
+        });
+      });
+  }
+
+  renderNews(){
+    const news_list = this.state.news.map( news => { //把state里的list of news通过mapping函数
+      //遍历一个一个的news
+      return (
+        // react里如果用list的话，要用一个key放react能够找到list里面变化的item
+        // 从而让react只在virtual dom里面只更新变化的元素，而不是更新整个list
+        <a className='list-group-item' key={news.digest} href="#">
+          <NewsCard news={news} />
+        </a>
+      );
+    });
+
+    return (
+      <div className="container-fluid">
+        <div className="list-group">
+          {news_list}
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    if (this.state.news) {
+      return(
+        <div>
+          {this.renderNews()}
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          Loading...
+        </div>
+      );
+    }
+  }
+}
+
+export default NewsPanel;
---
+++ project2/web_server/client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+
+ReactDOM.render(
+  <App />, //content
+  document.getElementById('root')// context
+)
